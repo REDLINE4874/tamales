2315bc9b898972eadc57edb2a419f7971af4078f
REDLINE <Rcandre48@gmail.com>
2026-07-22 21:46:48 -0600

    tamales proyect complet

--- web/config.js
+++ web/config.js
@@ -1,4 +1,4 @@
 // Pega aquí la URL de implementación de tu Apps Script (termina en /exec).
 // Extensiones → Apps Script → Implementar → Nueva implementación → Aplicación web
 // o, si usas clasp: npm run deploy && npm run open:webapp
-const API_URL = "https://script.google.com/macros/s/AKfycbxgbnhjvlT4yj8j1hwoROXW-2SekLbwpbe9DARre7RIpHtoyzJnrktq7XL7MESQMLE/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbygHCJlT48hz3Pdq47-KSfuPKYAY2X5xJlr43RhDsiGvBWydbIF0_3ZXB-231Yy4uRg/exec";
